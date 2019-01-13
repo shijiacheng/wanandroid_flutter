2fc640bc4de961c6719d8202a199604d64c91da8
shijiacheng <iot_shijiacheng@163.com>
2019-01-13 22:52:24 +0800

    跳转工具类抽出

---
+++ lib/utils/RouteUtil.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../ui/WebViewPageUI.dart';
+
+/// 路由工具类
+class RouteUtil{
+
+  ///跳转到webview打开
+  static void toWebView(BuildContext context,String title,String link) async {
+    await Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new WebViewPageUI(
+        title: title,
+        url: link,
+      );
+    }));
+  }
+}
