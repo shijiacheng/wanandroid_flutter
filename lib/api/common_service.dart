4d8d465480dee6cee7a8cf92842c895327693d39
shijiacheng <iot_shijiacheng@163.com>
2019-02-05 21:39:57 +0800

    api整体提交

--- lib/api/common_service.dart
+++ lib/api/common_service.dart
@@ -11,7 +11,12 @@ import '../model/NaviModel.dart';
 import '../model/ProjectListModel.dart';
 import '../model/ProjectTreeModel.dart';
 import '../model/HotWordModel.dart';
-
+import '../model/UserModel.dart';
+import '../model/CollectionModel.dart';
+import '../model/BaseModel.dart';
+import '../model/WebsiteCollectionModel.dart';
+import '../model/TodoListModel.dart';
+import '../common/User.dart';
 
 class CommonService{
   void getBanner(Function callback) async {
@@ -21,6 +26,12 @@ class CommonService{
   }
   void getArticleList(Function callback,int _page) async {
     DioManager.singleton.dio.get(Api.HOME_ARTICLE_LIST+"$_page/json", options: _getOptions()).then((response) {
+      callback(ArticleModel(response.data));
+    });
+  }
+
+  void getArticleList2(Function callback,int _page) async {
+    DioManager.singleton.dio.get("http://www.wanandroid.com/lg/collect/list/0/json", options: _getOptions()).then((response) {
       callback(ArticleModel(response.data));
     });
   }
@@ -83,18 +94,150 @@ class CommonService{
     });
   }
 
-  /// 获取搜索结果
+  /// 登录
   void login(Function callback,String _username,String _password) async {
     FormData formData = new FormData.from({
       "username": _username,
       "password":_password
     });
     DioManager.singleton.dio.post(Api.USER_LOGIN, data: formData, options: _getOptions()).then((response) {
-      callback(ArticleModel(response.data));
+      callback(UserModel(response.data),response);
+    });
+  }
+
+  /// 注册
+  void register(Function callback,String _username,String _password) async {
+    FormData formData = new FormData.from({
+      "username": _username,
+      "password":_password,
+      "repassword":_password
+    });
+    DioManager.singleton.dio.post(Api.USER_REGISTER, data: formData, options: null).then((response) {
+      callback(UserModel(response.data));
+    });
+  }
+
+  /// 获取收藏列表
+  void getCollectionList(Function callback,int _page) async {
+    DioManager.singleton.dio.get(Api.COLLECTION_LIST+"$_page/json", options: _getOptions()).then((response) {
+      callback(CollectionModel(response.data));
+    });
+  }
+
+  /// 我的收藏-取消收藏
+  void cancelCollection(Function callback,int _id,int _originId) async {
+     FormData formData = new FormData.from({
+      "originId": _originId
+    });
+    DioManager.singleton.dio.post(Api.CANCEL_COLLECTION+"$_id/json", data: formData,options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+  /// 我的收藏-新增收藏
+  void addCollection(Function callback,String _title,String _author,String _link) async {
+     FormData formData = new FormData.from({
+      "title": _title,
+      "author":_author,
+      "link":_link
+    });
+    DioManager.singleton.dio.post(Api.ADD_COLLECTION, data: formData,options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+
+  /// 网站收藏列表
+  void getWebsiteCollectionList(Function callback) async {
+    DioManager.singleton.dio.get(Api.WEBSITE_COLLECTION_LIST, options: _getOptions()).then((response) {
+      callback(WebsiteCollectionModel(response.data));
+    });
+  }
+  /// 取消网站收藏
+  void cancelWebsiteCollectionList(Function callback,int _id) async {
+    FormData formData = new FormData.from({
+      "id": _id,
+    });
+    DioManager.singleton.dio.post(Api.CANCEL_WEBSITE_COLLECTION, data: formData, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+  /// 新增网站收藏
+  void addWebsiteCollectionList(Function callback,String _name,String _link) async {
+    FormData formData = new FormData.from({
+      "name": _name,
+      "link":_link
+    });
+    DioManager.singleton.dio.post(Api.ADD_WEBSITE_COLLECTION, data: formData, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+  /// 编辑网站收藏
+  void editWebsiteCollectionList(Function callback,int _id,String _name,String _link) async {
+    FormData formData = new FormData.from({
+      "id": _id,
+      "name": _name,
+      "link":_link
+    });
+    DioManager.singleton.dio.post(Api.EDIT_WEBSITE_COLLECTION, data: formData, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+
+  /// todo列表
+  void getTodoList(Function callback,int _type) async {
+    DioManager.singleton.dio.get(Api.TODO_LIST+"$_type/json", options: _getOptions()).then((response) {
+      callback(TodoListModel(response.data));
+    });
+  }
+  
+  /// 新增todo数据
+  void addTodoData(Function callback,String _title,String _content,String _date,int _type) async {
+    FormData formData = new FormData.from({
+      "title": _title,
+      "content": _content,
+      "date":_date,
+      "type":_type
+    });
+    DioManager.singleton.dio.post(Api.ADD_TODO, data: formData, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+
+  /// 更新todo数据
+  void updateTodoData(Function callback,int _id,String _title,String _content,String _date,int _status,int _type) async {
+    FormData formData = new FormData.from({
+      "title": _title,
+      "content": _content,
+      "date":_date,
+      "status":_status,
+      "type":_type
+    });
+    DioManager.singleton.dio.post(Api.UPDATE_TODO+"$_id/json", data: formData, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+
+
+  /// 删除todo数据
+  void deleteTodoData(Function callback,int _id) async {
+    DioManager.singleton.dio.post(Api.DELETE_TODO+"$_id/json", data: null, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
+    });
+  }
+  
+  /// 仅更新todo完成状态
+  void doneTodoData(Function callback,int _id,int _status) async {
+    FormData formData = new FormData.from({
+      "status":_status
+    });
+    DioManager.singleton.dio.post(Api.DONE_TODO+"$_id/json", data: formData, options: _getOptions()).then((response) {
+      callback(BaseModel(response.data));
     });
   }
 
   Options _getOptions() {
-    return null;
+    Map<String,String> map = new Map();
+    List<String> cookies = User().cookie;
+    map["Cookie"] = cookies.toString();
+    return Options(headers: map);
   }
 }
